d9e4c73b7d1c8ed4ca8b295a008319a75ddaf434
Gustavo Girotto Queiroz <gustavo.g.queiroz2015@gmail.com>
2024-07-06 12:17:08 -0300

    Interaction test added with test runner

--- src/InboxScreen.stories.jsx
+++ src/InboxScreen.stories.jsx
@@ -4,6 +4,10 @@ import InboxScreen from "./InboxScreen";
 
 import { Default as TaskListDefault } from "./components/TaskList.stories";
 
+// The @storybook/test package replaces the @storybook/jest and @storybook/testing-library testing packages,
+// offering a smaller bundle size and a more straightforward API based on the Vitest package.
+import { expect, userEvent, findByRole, within } from '@storybook/test';
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -33,5 +37,60 @@ export const Error = {
         }),
       ],
     },
+  },
+};
+
+export const PinTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  // For Interaction Tests - Check Interactions Panel in Storybook
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (id) => canvas.findByRole('listitem', { name: id });
+
+    const itemToPin = await getTask('task-4');
+    // Find the pin button
+    const pinButton = await findByRole(itemToPin, 'button', { name: 'pin' });
+    // Click the pin button
+    await userEvent.click(pinButton);
+    // Check that the pin button is now a unpin button
+    const unpinButton = within(itemToPin).getByRole('button', {
+      name: 'unpin',
+    });
+    await expect(unpinButton).toBeInTheDocument();
+  },
+};
+
+export const ArchiveTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (id) => canvas.findByRole('listitem', { name: id });
+
+    const itemToArchive = await getTask('task-2');
+    const archiveButton = await findByRole(itemToArchive, 'button', {
+      name: 'archiveButton-2',
+    });
+    await userEvent.click(archiveButton);
+  },
+};
+
+export const EditTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (id) => canvas.findByRole('listitem', { name: id });
+
+    const itemToEdit = await getTask('task-5');
+    const taskInput = await findByRole(itemToEdit, 'textbox');
+    await userEvent.type(taskInput, ' and disabled state');
+    await expect(taskInput.value).toBe(
+      'Fix bug in input error state and disabled state'
+    );
   },
 };
